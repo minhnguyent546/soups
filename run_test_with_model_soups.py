eeb92d1baf90699c9283e7e60daa9252018d7de5
minhnguyent546 <minhnguyent546@gmail.com>
2025-06-19 14:26:13 +0700

    rename files

---
+++ run_test_with_model_soups.py
@@ -0,0 +1,4 @@
+from soups.run_test_with_model_soups import main
+
+if __name__ == '__main__':
+    main()
--- test_with_model_soups.py
+++
@@ -1,4 +0,0 @@
-from soups.test_with_model_soups import main
-
-if __name__ == '__main__':
-    main()
