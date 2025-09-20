de3804c0835f0e7a05fef7e574faadc4fc8e2cf9
minhnguyent546 <minhnguyent546@gmail.com>
2025-09-20 10:09:26 +0700

    fix: test incorrect `best_val_f1_checkpoint_test_results`

--- soups/run_test_multiple_checkpoints.py
+++ soups/run_test_multiple_checkpoints.py
@@ -139,9 +139,9 @@ def test_multiple_checkpoints(args: argparse.Namespace) -> None:
     test_data['best_val_f1_checkpoint_test_results'] = test_data[best_val_f1_checkpoint_path][
         'test'
     ]
-    test_data['best_val_f1_checkpoint_test_results'] = test_data[best_val_f1_checkpoint_path][
-        'checkpoint_path'
-    ] = best_val_f1_checkpoint_path
+    test_data['best_val_f1_checkpoint_test_results']['checkpoint_path'] = (
+        best_val_f1_checkpoint_path
+    )
 
     test_data[f'best_test_results@{len(checkpoint_paths)}'] = convert_eval_results_to_dict(
         eval_results=best_test_results, class_names=class_names
